3db66cb438c07f0cd56cd5568d49a66a26d56c09
Zille <zillerahim3@gmail.com>
2021-05-10 21:29:31 +1000

    made hero banner + store info cards

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,5 @@
 function openSearchBar() {
     console.log("search icon clicked");
     var icon = document.getElementById("searchBox");
-    console.log(icon);
-    console.log(icon.classList);
-    icon.classList.replace("search-box-not-opened", "search-box");
-    
+    icon.style.display = "block";
 }
