abc090e0b53200f48c31e9b17592822a8074f576
Zille <zillerahim3@gmail.com>
2021-06-14 05:40:02 +1000

    added some flex-wrap

--- js/navbar.js
+++ js/navbar.js
@@ -2,4 +2,9 @@ function openSearchBar() {
     console.log("search icon clicked");
     var icon = document.getElementById("searchBox");
     icon.style.display = "block";
+    document.getElementById("searchIcon").href = "./pages/product-listing.html";
+}
+
+function openProductListing() {
+    document.getElementById("searchIcon").href = "./pages/product-listing.html";
 }
